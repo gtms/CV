0a7d2adf73959c4d51658a41f346390e07a5eb86
Gil <gil.tms@gmail.com>
2014-09-30 21:13:43 +0200

    Update bash/compile.sh

--- bash/compile.sh
+++ bash/compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # compiles cv.tex with xelatex and moves output files to appropriate directories
 
-cd /home/gil/Documents/CV/tex
-xelatex cv.tex
+cd ~/Dropbox/personal/CV/tex
+xelatex cv
+bibtex cv
+xelatex cv
+xelatex cv
 mv cv.pdf ../pdf/
 mv *.aux *.log *.out ./log/
